3641f2d081db2788879678fca0bffeb912ab54de
Zidane-Fauzan <zidanefauzan75@gmail.com>
2025-02-10 01:47:41 +0700

    Update Hompage

--- moovid2025/src/components/Footer.jsx
+++ moovid2025/src/components/Footer.jsx
@@ -128,7 +128,7 @@ const Footer = () => {
       <div className="mt-2 sm:flex sm:items-center sm:justify-between">
 
         <p className="mt-2 text-center text-sm text-gray-500 sm:mt-0 sm:text-right">
-          Copyright &copy; 2022. All rights reserved.
+          Test &copy; 2022. All rights reserved.
         </p>
       </div>
     </div>
